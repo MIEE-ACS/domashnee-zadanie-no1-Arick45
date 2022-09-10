e2a38fd5c1b05e6e7fdd55e7105a58f002c627af
Arick45 <39058961+Arick45@users.noreply.github.com>
2022-09-10 18:30:54 +0300

    -

---
+++ homework1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите температуру в Цельсиях TC: ");
+            float TC = float.Parse(Console.ReadLine());
+            if (TC >= -273.15f)// проверка на существование такой температуры
+            {
+                float TF = TC * 9 / 5 + 32;//перевод в фаренгейты
+                Console.WriteLine($"Температура в Фаренгейтах TF: {Math.Round(TF, 3)}F");
+            }
+            else
+            {
+                Console.WriteLine("Невозможное значение температуры в Цельсиях.Программа завершит работу");
+            }
+            Console.ReadKey(); // Ожидание ввода для того, что бы консоль не закрывалась.
+        }
+    }
+}
+
